6ce39a0e0c6a94886edffcdc50a7d340b697cbe9
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2022-01-12 15:48:39 +0100

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,4 @@ void main() {
   runApp(const MyApp());
 }
 
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+          
